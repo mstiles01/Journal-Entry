a661170a26754680760c68a8b6664022bb09fc0c
Michael Stiles <mstiles01@icloud.com>
2019-07-18 13:55:59 -0500

    Added DOM elements via JS

--- scripts/journal.js
+++ scripts/journal.js
@@ -1,12 +1,28 @@
 const journalEntries = []
 
+
+const addEntry = (journalEntry) =>{
+journalEntries.push(journalEntry)}
+
+
+const puttingentrytoDOM = document.querySelector("#entryLog")
+
+const makeJournalEntryComponent = (journalEntry) => {
+   const journalEntryTemplate = `
+    <article class="entryLog">
+    <h2>${journalEntry.Date}</h2>
+    <h3>${journalEntry.Concepts}</h3>
+    <h3>${journalEntry.Journal}</h3>
+    <h3>${journalEntry.Mood}</h3>
+    </article>`
+    
+    puttingentrytoDOM.innerHTML += journalEntryTemplate
+}
+
 const journalEntry1 = {
     Date:"7/13/19",
     Concepts:"Objects, Arrays, Dot notation, Bracket notation",
     Journal:"I'm confused",
     Mood:"Sad"
 }
-
-journalEntries.push(journalEntry1)
-
-console.log(journalEntries)
+makeJournalEntryComponent(journalEntry1)
