33a9914f4c04651634284dd49e43d9017d5b41bd
Michael Stiles <mstiles01@icloud.com>
2019-07-20 12:43:55 -0500

    Styled Jounral Entry Project

--- scripts/journal.js
+++ scripts/journal.js
@@ -5,14 +5,14 @@ const addEntry = (journalEntry) =>{
 journalEntries.push(journalEntry)}
 
 
-const puttingentrytoDOM = document.querySelector("#entryLog")
+const puttingentrytoDOM = document.querySelector(".entries")
 
 const makeJournalEntryComponent = (journalEntry) => {
    const journalEntryTemplate = `
     <article class="entryLog">
     <h2>${journalEntry.Date}</h2>
-    <h3>${journalEntry.Concepts}</h3>
-    <h3>${journalEntry.Journal}</h3>
+    <h3>${journalEntry.Topic}</h3>
+    <h3>${journalEntry.Entry}</h3>
     <h3>${journalEntry.Mood}</h3>
     </article>`
     
@@ -21,8 +21,17 @@ const makeJournalEntryComponent = (journalEntry) => {
 
 const journalEntry1 = {
     Date:"7/13/19",
-    Concepts:"Objects, Arrays, Dot notation, Bracket notation",
-    Journal:"I'm confused",
+    Topic:"Objects, Arrays, Dot notation, Bracket notation",
+    Entry:"I'm confused",
     Mood:"Sad"
 }
+
+const journalEntry2 = {
+    Date: "7/20/2019",
+    Topic: "Test",
+    Entry: "Test",
+    Mood: "Happy"
+}
 makeJournalEntryComponent(journalEntry1)
+makeJournalEntryComponent(journalEntry2)
+
