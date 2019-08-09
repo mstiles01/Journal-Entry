ae878327ecbbaae554b2d043d24a45f5f0c072c6
Michael Stiles <mstiles01@icloud.com>
2019-08-09 10:16:19 -0500

    Primitave Delete

--- scripts/entriesDOM.js
+++ scripts/entriesDOM.js
@@ -1,10 +1,16 @@
+import HTMLRep from "./entryComponent.js"
+
 // Refernce to DOM element to where we put out data
 const placingEntries = document.querySelector(".entries")
 // This is rendering 
 const displayEntriesInDOM = (entriesHTMLRep) => {
-    placingEntries.innerHTML += entriesHTMLRep
-}
+    // entriesDIV.innerHTML = ""
+    // for (let currentEntry of arrayOfEntries) {
+    //     let convertedJournal = HTMLRep.createJournalEntryComponent(currentEntry);
+        placingEntries.innerHTML += entriesHTMLRep
 
-export default {
-    placingEntries, displayEntriesInDOM
-}
+    }
+
+    export default {
+        placingEntries, displayEntriesInDOM
+    }
